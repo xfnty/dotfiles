389c1a83a66e997bf7a5779ecdabb79149a664bd
xfnty <tsentsevitsky.v@gmail.com>
2023-06-04 12:06:20 +0700

    Added dwmblocks scripts

---
+++ system/dwmblocks/usr/local/bin/dwmblocks-util.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Utility functions for theming statudwmblocksar scripts.
+
+display() {
+    if [ -n "$2" ]; then
+        color="$2"
+    else
+        case "$(basename "$0")" in
+            dwmblocks-memory) color=15 ;;
+            dwmblocks-date) color=12 ;;
+            *) color=15 ;;
+        esac
+    fi
+
+    case "$STATUSBAR" in
+        "dwmblocks")
+            echo "^C$color^$1"
+            ;;
+        *)
+            echo "$1"
+            ;;
+    esac
+}
