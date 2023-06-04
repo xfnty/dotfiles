9d91d7e2233772c0647abd90662e2c6cf7a9fb38
xfnty <tsentsevitsky.v@gmail.com>
2023-06-04 13:03:51 +0700

    Minor changes

--- system/dwmblocks/usr/local/bin/dwmblocks-util.sh
+++ system/dwmblocks/usr/local/bin/dwmblocks-util.sh
@@ -2,22 +2,24 @@
 # Utility functions for theming statudwmblocksar scripts.
 
 display() {
-    if [ -n "$2" ]; then
-        color="$2"
-    else
-        case "$(basename "$0")" in
-            dwmblocks-memory) color=15 ;;
-            dwmblocks-date) color=12 ;;
-            *) color=15 ;;
-        esac
-    fi
+    # if [ -n "$2" ]; then
+    #     color="$2"
+    # else
+    #     case "$(basename "$0")" in
+    #         dwmblocks-memory) color=15 ;;
+    #         dwmblocks-date) color=12 ;;
+    #         *) color=15 ;;
+    #     esac
+    # fi
+    # 
+    # case "$STATUSBAR" in
+    #     "dwmblocks")
+    #         echo "^C$color^$1"
+    #         ;;
+    #     *)
+    #         echo "$1"
+    #         ;;
+    # esac
 
-    case "$STATUSBAR" in
-        "dwmblocks")
-            echo "^C$color^$1"
-            ;;
-        *)
-            echo "$1"
-            ;;
-    esac
+    echo "$1"
 }
